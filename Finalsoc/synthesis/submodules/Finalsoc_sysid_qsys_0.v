3f70650027cd4856af0804e927a7ab8e08f0ee50
njjt <shenjinzhi18@gmail.com>
2021-11-17 21:29:19 -0600

    tetris left right

--- Finalsoc/synthesis/submodules/Finalsoc_sysid_qsys_0.v
+++ Finalsoc/synthesis/submodules/Finalsoc_sysid_qsys_0.v
@@ -49,7 +49,7 @@ module Finalsoc_sysid_qsys_0 (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1636754207 : 0;
+  assign readdata = address ? 1637198166 : 0;
 
 endmodule
 
